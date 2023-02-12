05ea494041adc60376df8a17f301d02ff370c562
Nathan Laha <nlaha@outlook.com>
2023-02-11 16:06:48 -0800

    add changes

--- addons/dialogue_manager/dialogue_label.gd
+++ addons/dialogue_manager/dialogue_label.gd
@@ -10,7 +10,7 @@ const DialogueLine = preload("res://addons/dialogue_manager/dialogue_line.gd")
 
 
 ## The action to press to skip typing
-@export var skip_action: String = "ui_cancel"
+@export var skip_action: String = "ui_accept"
 
 ## The speed with which the text types out
 @export var seconds_per_step: float = 0.02
@@ -18,12 +18,30 @@ const DialogueLine = preload("res://addons/dialogue_manager/dialogue_line.gd")
 ## When off, the label will grow in height as the text types out
 @export var start_with_full_height: bool = true
 
+@export var log_mode: bool = false
+
+@export var gpt_mode: bool = false
+
+@export var type_sound: AudioStream
+var sfx_player
+
+var line_separator = "\n\n[color=white]>[/color] "
+
+var log_buffer: Array[String] = [""]
+var log_line_start = 0
+
+var bbcode_regex = RegEx.new()
 
 var dialogue_line: DialogueLine:
 	set(next_dialogue_line):
 		dialogue_line = next_dialogue_line
 		custom_minimum_size = Vector2.ZERO
-		text = dialogue_line.text
+		if not log_mode:
+			text = dialogue_line.text
+		else:
+			log_buffer.append(dialogue_line.text)
+			text = line_separator.join(PackedStringArray(log_buffer))
+			log_line_start = get_total_character_count() - bbcode_regex.sub(dialogue_line.text, "").length()
 	get:
 		return dialogue_line
 
@@ -33,17 +51,38 @@ var waiting_seconds: float = 0
 var is_typing: bool = false
 var has_finished: bool = false
 
+func push_response(response):
+	if log_mode:
+		log_buffer.append("[color=#30D5E0]" + response + "[/color]")
+		text = line_separator.join(PackedStringArray(log_buffer))
+		log_line_start = get_total_character_count()
+
+func _ready():
+	bbcode_regex.compile("\\[\\\\?\\w*\\]")
+	sfx_player = AudioStreamPlayer2D.new()
+	add_child(sfx_player)
 
 func _process(delta: float) -> void:
+	
+	if not is_typing:
+		var lstick = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+		
+		if lstick.y > 0.2 and get_v_scroll_bar().ratio < 1.0:
+			get_v_scroll_bar().ratio += lstick.y * delta
+		if lstick.y < -0.2 and get_v_scroll_bar().ratio > 0.0:
+			get_v_scroll_bar().ratio += lstick.y * delta
+	else:
+		get_v_scroll_bar().ratio = 1.0
+		
 	if is_typing:
 		# Type out text
 		if visible_ratio < 1:
 			# If cancel is pressed then skip typing it out
 			if Input.is_action_just_pressed(skip_action):
 				# Run any inline mutations that haven't been run yet
-				for i in range(visible_characters, get_total_character_count()):
+				for i in range(visible_characters - log_line_start, get_total_character_count()):
 					mutate_inline_mutations(i)
-				visible_characters = get_total_character_count()
+				visible_characters = log_line_start + get_total_character_count()
 				has_finished = true
 				emit_signal("finished_typing")
 				return
@@ -62,13 +101,34 @@ func _process(delta: float) -> void:
 
 
 func reset_height() -> void:
-	assert(false, "DialogueLabel.reset_height() has been replaced with the new RichTextLabel \"fit_content\" property. You can remove any calls to reset_height().")
+	# For some reason, RichTextLabels within containers don't resize properly when their content 
+	# changes so we make a clone that isn't bound by a VBox
+	var size_check_label: RichTextLabel = duplicate(DUPLICATE_USE_INSTANTIATION)
+	size_check_label.modulate.a = 0
+	size_check_label.anchor_left = 1
+	get_tree().current_scene.add_child(size_check_label)
+	size_check_label.size = Vector2(size.x, 0)
+	
+	if start_with_full_height:
+		# Give the size check a chance to resize
+		await get_tree().process_frame
+	
+	# Resize our dialogue label with the new size hint
+	custom_minimum_size = size_check_label.size
+	size = Vector2.ZERO
+	
+	# Destroy our clone
+	size_check_label.free()
 
 
 # Start typing out the text
 func type_out() -> void:
-	text = dialogue_line.text
-	visible_characters = 0
+	if not log_mode:
+		text = dialogue_line.text
+		visible_characters = 0
+	else:
+		text = line_separator.join(PackedStringArray(log_buffer))
+		visible_characters = log_line_start
 	has_finished = false
 	waiting_seconds = 0
 	
@@ -82,7 +142,7 @@ func type_out() -> void:
 		# Run any inline mutations
 		for i in range(0, get_total_character_count()):
 			mutate_inline_mutations(i)
-		visible_characters = get_total_character_count()
+		visible_characters += get_total_character_count()
 		emit_signal("finished_typing")
 	else:
 #		percent_per_index = 100.0 / float(get_total_character_count()) / 100.0
@@ -91,25 +151,36 @@ func type_out() -> void:
 
 # Type out the next character(s)
 func type_next(delta: float, seconds_needed: float) -> void:
-	if visible_characters == get_total_character_count():
+	if visible_characters - log_line_start == get_total_character_count():
 		return
 	
-	if last_mutation_index != visible_characters:
-		last_mutation_index = visible_characters
-		mutate_inline_mutations(visible_characters)
+	if last_mutation_index != visible_characters - log_line_start:
+		last_mutation_index = visible_characters - log_line_start
+		mutate_inline_mutations(visible_characters - log_line_start)
 	
-	if last_wait_index != visible_characters and get_pause(visible_characters) > 0:
-		last_wait_index = visible_characters
-		waiting_seconds += get_pause(visible_characters)
-		emit_signal("paused_typing", get_pause(visible_characters))
+	if last_wait_index != visible_characters - log_line_start and get_pause(visible_characters - log_line_start) > 0:
+		last_wait_index = visible_characters - log_line_start
+		waiting_seconds += get_pause(visible_characters - log_line_start)
+		emit_signal("paused_typing", get_pause(visible_characters - log_line_start))
 	else:
 #		visible_ratio += percent_per_index
-		visible_characters += 1
-		seconds_needed += seconds_per_step * (1.0 / get_speed(visible_characters))
+		var add_num = 1
+		if gpt_mode:
+			add_num = clampi(randi_range(1, 3), 1, get_total_character_count() - visible_characters)
+		visible_characters += add_num
+		seconds_needed += seconds_per_step * (1.0 / get_speed(visible_characters - log_line_start))
 		if seconds_needed > delta:
 			waiting_seconds += seconds_needed
-			if visible_characters < get_total_character_count():
-				emit_signal("spoke", text[visible_characters - 1], visible_characters - 1, get_speed(visible_characters))
+			if visible_characters - log_line_start < get_total_character_count():
+				sfx_player.stream = type_sound
+				sfx_player.pitch_scale = randf_range(0.95, 1.05)
+				sfx_player.play()
+				emit_signal(
+					"spoke", 
+					text[visible_characters - log_line_start - 1], 
+					visible_characters - log_line_start - 1, 
+					get_speed(visible_characters - log_line_start)
+				)
 		else:
 			type_next(delta, seconds_needed)
 
